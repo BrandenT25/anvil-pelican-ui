a2684506fc71284dcf5c7f6a229b4365755488f7
BrandenT25 <brandent35@tamu.edu>
2026-07-15 15:39:28 -0400

    added problem with uv sync not running

---
+++ .uv-bin/env.fish
@@ -0,0 +1,4 @@
+if not contains "/anvil/projects/x-cda090008/anvil_reu26_pj4/pelican-ui/bin/../.uv-bin" $PATH
+    # Prepending path in case a system-installed binary needs to be overridden
+    set -x PATH "/anvil/projects/x-cda090008/anvil_reu26_pj4/pelican-ui/bin/../.uv-bin" $PATH
+end
