0c9083aabdfcf5d2301bee7b21a6505b853950d3
BrandenT25 <brandent35@tamu.edu>
2026-07-15 15:44:35 -0400

    updated passenger wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,10 @@
+import os
 import sys
 import site
 
-venv_site = '/home/x-yourusername/ondemand/dev/pelican-ui/.venv/lib/python3.9/site-packages'
+venv_site = os.path.join(
+    os.environ['HOME'], 'ondemand/dev/anvil-pelican-ui/.venv/lib/python3.11/site-packages'
+)
 site.addsitedir(venv_site)
 
 sys.path.insert(0, venv_site)
